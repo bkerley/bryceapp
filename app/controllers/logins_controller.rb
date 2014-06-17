b1404607c1c04ffb49ecf839f6069712fe792939
Bryce Kerley <bkerley@brycekerley.net>
2014-06-17 16:44:56 -0400

    login form works

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,4 +1,17 @@
 class LoginsController < ApplicationController
   def show
   end
+
+  def create
+    u = User.
+      find_by(username: params[:username]).
+      try(:authenticate, params[:password])
+
+    if u
+      session[:user_id] = u.id
+      return redirect_to tweets_path
+    end
+
+    redirect_to(login_path, notice: 'wrong login info, try again')
+  end
 end
